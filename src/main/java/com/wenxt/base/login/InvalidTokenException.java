d0ce564e5f05d69052b73a08cc62d4209ceb1e0a
ragul-wenxt <ragul.arulkumaran@wenxttech.com>
2025-08-06 18:14:46 +0530

    as

--- src/main/java/com/wenxt/base/login/InvalidTokenException.java
+++
@@ -1,9 +0,0 @@
-package com.wenxt.base.login;
-
-public class InvalidTokenException extends Exception {
-
-	public InvalidTokenException(String string) {
-		// TODO Auto-generated constructor stub
-	}
-
-}
